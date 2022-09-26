2ae018256a1541cb682eff5b360e027d434f5c52
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-27 00:12:08 +0100

    Add test to doctor.rb files

--- spec/controller/doctor_spec.rb
+++ spec/controller/doctor_spec.rb
@@ -1,85 +1,28 @@
-require 'swagger_helper'
+require 'rails_helper'
 
-describe 'Doctors API' do
-  path 'api/v1/doctors' do
-    get 'Get all doctors' do
-      tags 'Doctors'
-      produces 'application/json'
-      response '200', 'doctors found' do
-        run_test!
-      end
-    end
+RSpec.describe Doctor, type: :model do
+  it 'is not valid without a name' do
+    doctor = Doctor.new(name: nil)
+    expect(doctor).to_not be_valid
   end
-  path 'api/v1/doctors' do
-    post 'Create a doctor' do
-      tags 'Doctors'
-      consumes 'application/json'
-      parameter name: :doctor, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          specialty: { type: :string }
-        },
-        required: %w[name specialty]
-      }
-      response '201', 'doctor created' do
-        let(:doctor) { { name: 'Abiodun', specialty: 'Raheem' } }
-        # run_test!
-      end
-      response '422', 'invalid request' do
-        let(:doctor) { { name: 'Stanley' } }
-        # run_test!
-      end
-    end
+
+  it 'is not valid without a speciality' do
+    doctor = Doctor.new(speciality: nil)
+    expect(doctor).to_not be_valid
   end
-  path 'api/v1/doctors/{id}' do
-    get 'Get a doctor' do
-      tags 'Doctors'
-      produces 'application/json'
-      parameter name: :id, in: :path, type: :string, required: true
-      response '200', 'doctor found' do
-        let(:id) { create(:doctor).id }
-        # run_test!
-      end
-      response '404', 'doctor not found' do
-        let(:id) { 'invalid' }
-        # run_test!
-      end
-    end
+
+  it 'is not valid without a location' do
+    doctor = Doctor.new(location: nil)
+    expect(doctor).to_not be_valid
   end
-  path 'api/v1/doctors/{id}' do
-    put 'Update a doctor' do
-      tags 'Doctors'
-      consumes 'application/json'
-      parameter name: :id, in: :path, type: :string, required: true
-      parameter name: :doctor, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          specialty: { type: :string }
-        },
-        required: %w[name specialty]
-      }
-      response '201', 'doctor updated' do
-        let(:id) { create(:doctor).id }
-        let(:doctor) { { name: 'Moises', specialty: 'Pediatria' } }
-        # run_test!
-      end
-      response '422', 'invalid request' do
-        let(:id) { create(:doctor).id }
-        let(:doctor) { { name: 'Miller' } }
-        # run_test!
-      end
-    end
+
+  it 'is not valid without an email' do
+    doctor = Doctor.new(email: nil)
+    expect(doctor).to_not be_valid
   end
-  path 'api/v1/doctors/{id}' do
-    delete 'Delete a doctor' do
-      tags 'Doctors'
-      parameter name: :id, in: :path, type: :string, required: true
-      response '204', 'doctor deleted' do
-        let(:id) { create(:doctor).id }
-        # run_test!
-      end
-    end
+
+  it 'is not valid without a bill' do
+    doctor = Doctor.new(bill: nil)
+    expect(doctor).to_not be_valid
   end
 end
