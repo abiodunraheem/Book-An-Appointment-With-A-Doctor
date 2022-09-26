3dd7fff2d5550181cc7ec8f33f1559716289c6d6
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-26 16:18:44 +0200

    Remove string column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_26_135425) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_141701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_135425) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
