f91c1c0eeb35e29cb6c09b6e552304a6ca78a207
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-26 16:35:24 +0100

    Add method to create doctors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,10 +1,24 @@
 class Api::V1::DoctorsController < ApplicationController
   def index
+    @doctors = Doctor.where(user_id: params[:user_id])
+    render json: @doctors
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @doctor = Doctor.new(doctor_params)
+    @doctor.user_id = @user.id
+    if @doctor.save
+      render json: { status: 201, message: 'doctor created successfully!', content: { doctor: @doctor } }
+    else
+      render json: { error: 401, message: ' doctor cannot be processed !' }
+    end
   end
 
-  def destroy
+  
+  private
+
+  def doctor_params
+    params.permit(:name, :speciality, :bill, :image, :location, :email)
   end
 end
