04164bf0d31208fe2adca1e02816dd58969721a0
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-26 18:02:12 +0100

    Add test for User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with valid attributes" do
+    user = User.new(username: "john Doe")
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a username" do
+    user = User.new(username: nil)
+    expect(user).to_not be_valid
+  end
+end
+
+
