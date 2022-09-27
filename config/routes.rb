e91e4a3132f3020a49a724a38f035b205d56c061
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-27 16:42:25 +0100

    Add reservations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  get 'reservations/show'
+  get 'reservations/create'
   post 'api/v1/register/:username/:email', to: 'users#register'
   get 'api/v1/login/:username', to: 'users#login'
   namespace :api do
     namespace :v1 do
-      get 'pages/index'
+      get 'pages/splash'
       resources :doctors, only: [:index, :create, :destroy]
     end
   end
