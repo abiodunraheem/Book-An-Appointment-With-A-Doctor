8b58b78822e8e105cd5b032a29440fe91312fade
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-27 12:10:15 +0200

    Add the user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
       sessions: 'users/sessions',
       registrations: 'users/registrations'
   }
-get '/member-data', to: 'members#show'
   namespace :api do
     namespace :v1 do
       resources :doctors, only: [:index, :create, :destroy]
