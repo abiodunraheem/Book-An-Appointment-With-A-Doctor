80a00d7b678d447f6e5851446a16aa9b373ad85e
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-06 22:09:18 +0200

    Add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is valid with valid attributes' do
-    user = User.new(username: 'john Doe')
+    user = User.new(username: 'john Doe', email:"john@test.com")
     expect(user).to be_valid
   end
 
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
   end
 
   it 'is not valid without an email' do
-    user = User.new(username: nil)
+    user = User.new(email: nil)
     expect(user).to_not be_valid
   end
 end
