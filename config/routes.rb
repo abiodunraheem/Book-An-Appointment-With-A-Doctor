fcb27bfe20d84bf30adb044ac376d6bdaf43d320
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-06 12:51:47 +0200

    Add and update user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,4 @@ Rails.application.routes.draw do
       resources :doctors, only: [:index, :create, :destroy]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
