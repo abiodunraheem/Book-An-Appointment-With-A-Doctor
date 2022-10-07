26aefdba3aafc3424a706bf3632cdff0ced34454
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-07 10:48:59 +0200

    Add tests to the doctors

--- spec/requests/api/v1/doctors_spec.rb
+++ spec/requests/api/v1/doctors_spec.rb
@@ -1,10 +1,35 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Api::V1::Doctors', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/api/v1/doctors'
-      expect(response).to have_http_status(:success)
+RSpec.describe 'api/v1/doctors', type: :request do
+  path '/api/v1/doctors' do
+    get('list doctors') do
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = { 'application/json' => {
+            example: JSON.parse(response.body, symbolize_names: true)
+          } }
+        end
+        run_test!
+      end
+    end
+    post('create doctor') do
+      response(200, 'successful') do
+        consumes 'appication/json'
+        parameter name: :doctor, in: :body, schema: {
+          type: :object,
+          properties: { name: { type: :string }, speciality: { type: :string }, avatar: { type: :string },
+                        description: { type: :string }, bill: { type: :integer }, email: { type: :string },
+                        location: { type: :string } },
+          required: %w[name speciality image description bill email location]
+        }
+        let(:doctor) { { name: 'doctor' } }
+        after do |example|
+          example.metadata[:response][:content] = { 'application/json' => {
+            example: JSON.parse(response.body, symbolize_names: true)
+          } }
+        end
+        run_test!
+      end
     end
   end
 end
